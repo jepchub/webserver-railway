ef17c695cbd0a5b0f6ccf6474126db1ca3006760
Jepc <joseenrique.pc21@gmail.com>
2023-01-30 12:38:40 -0400

    ADD: serve static content

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const app = express();
 
 const port = 8080;
 
+// servir contenido estático
+app.use(express.static('public'));
+
 // Ruta home
-app.get('/', function (req, res) {
-  res.send('Home page');
-});
+// app.get('/', function (req, res) {
+//   res.send('Home page');
+// });
 
 // Ruta hola-mundo
 app.get('/hola-mundo', function (req, res) {
@@ -15,7 +18,8 @@ app.get('/hola-mundo', function (req, res) {
 
 // si el cliente intenta acceder a alguna pagina que no este en las alguna de las rutas permitidas de la app le saldra un mensaje con un error 404
 app.get('*', (req, res) => {
-  res.send('404 / Page not found');
+  // res.send('404 / Page not found');
+  res.sendFile( __dirname + '/public/error404.html');
 });
 
 app.listen(port, () => {
---
+++ public/info.md
@@ -0,0 +1,2 @@
+#Info
+ - Aqui va el contenido que cualquier usuario que tenga el url puede ver
