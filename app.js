aec83557280fca6c415394b9e1d345c7e5ce19cc
Jepc <joseenrique.pc21@gmail.com>
2023-01-31 17:30:30 -0400

    ADD: serve a complete website

--- app.js
+++ app.js
@@ -12,14 +12,35 @@ app.use(express.static('public'));
 // });
 
 // Ruta hola-mundo
-app.get('/hola-mundo', function (req, res) {
-  res.send('Hola mundo en su respectiva ruta');
+// app.get('/hola-mundo', function (req, res) {
+//   res.send('Hola mundo en su respectiva ruta');
+// });
+
+// DEV Ruta index
+// app.get('/index', (req, res) => {
+
+//   res.sendFile( __dirname + '/public/index.html');
+
+// });
+// DEV Ruta generic
+app.get('/generic', (req, res) => {
+
+  res.sendFile( __dirname + '/public/generic.html')
+
+});
+// DEV Ruta elements
+app.get('/elements', (req, res) => {
+
+  res.sendFile( __dirname + '/public/elements.html')
+
 });
 
 // si el cliente intenta acceder a alguna pagina que no este en las alguna de las rutas permitidas de la app le saldra un mensaje con un error 404
 app.get('*', (req, res) => {
-  // res.send('404 / Page not found');
-  res.sendFile( __dirname + '/public/error404.html');
+  // res.send('404 / Page not found');//solo envia texto es decir manda esa informacion
+  // sendfile manda el archivo en la ruta que le especifiquemos
+  // __dirname es todo el path donde se encuentra la aplicasion, donde esta corriendo
+  res.sendFile( __dirname + '/public/error404.html')
 });
 
 app.listen(port, () => {
