1023c1bec0b166cdaf2dd1464f7eff26170b72ea
jepchub <53791166+jepchub@users.noreply.github.com>
2023-02-21 21:07:20 -0400

    ADD: production

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const hbs = require('hbs');
+require('dotenv').config();
 
 const app = express();
-const port = 8080;
+// const port = 8080;
+const port = process.env.PORT;
 
 // DEV Handlebars
 app.set('view engine', 'hbs');
